a6241824d6c95f9922d1f2d249653ce2999f40a3
maryamnasir65834 <96259491+maryamnasir65834@users.noreply.github.com>
2024-07-23 08:45:15 +0500

    Add files via upload

---
+++ HTML CSS/program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://127.0.0.1:5500") // Allow your frontend origin
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+// Use CORS middleware
+app.UseCors("AllowSpecificOrigin");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
